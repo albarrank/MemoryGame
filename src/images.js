cd4b46966cfee393c14c43310ec90313fb7d68c5
Jack Frost <albarrankevin97@gmail.com>
2019-06-10 17:05:31 -0700

    almsot done

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import beth from './images/kermit2.png';
+import birdperson from './images/kermit1.png';
+import evilmorty from './images/kermit3.png';
+import gianthead from './images/kermit4.png';
+import goldenford from './images/kermit5.png';
+import jerry from './images/kermit6.png';
+import jessica from './images/kermit7.png';
+import meeseeks from './images/kermit8.png';
+import morty from './images/kermit9.png';
+import mr from './images/kermit10.png';
+import rick from './images/kermit11.png';
+import summer from './images/kermit12.png';
+
+const images = [
+  beth,
+  birdperson,
+  evilmorty,
+  gianthead,
+  goldenford,
+  jerry,
+  jessica,
+  meeseeks,
+  morty,
+  mr,
+  rick,
+  summer
+];
+
+export default images;
