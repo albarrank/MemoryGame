1945999e9588dad5f4432c84588d2c4f084c5fc0
Jack Frost <albarrankevin97@gmail.com>
2019-06-12 13:39:21 -0700

    Updates

---
+++ precache-manifest.360d5016a013de0efb6932030e65dce0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9770b0f89e5e14a07567136bcc11ef88",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "bebfd280d776bb79eb80",
+    "url": "/clicky/static/css/main.a636e895.chunk.css"
+  },
+  {
+    "revision": "015be0247dc3ee2955cc",
+    "url": "/clicky/static/js/2.76d8ed81.chunk.js"
+  },
+  {
+    "revision": "bebfd280d776bb79eb80",
+    "url": "/clicky/static/js/main.b96a7156.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  },
+  {
+    "revision": "9a30e6c6c75344e5bd217bbe9720ef3e",
+    "url": "/clicky/static/media/bg.9a30e6c6.png"
+  },
+  {
+    "revision": "97fbebb6422674c924bcf1d87dff93aa",
+    "url": "/clicky/static/media/kermit1.97fbebb6.png"
+  },
+  {
+    "revision": "028e72954fa800c61c915b10b75a3e82",
+    "url": "/clicky/static/media/kermit10.028e7295.png"
+  },
+  {
+    "revision": "f44bbab5a9412e5f4d1a51c80b0cc3d6",
+    "url": "/clicky/static/media/kermit11.f44bbab5.png"
+  },
+  {
+    "revision": "67eb2b2b073794d9eb18df6d9eea220b",
+    "url": "/clicky/static/media/kermit12.67eb2b2b.png"
+  },
+  {
+    "revision": "216a5cc03d80338789904576c2ad4c0d",
+    "url": "/clicky/static/media/kermit2.216a5cc0.png"
+  },
+  {
+    "revision": "eab3bf7f8b1edff3f6ba3bef9ac7109c",
+    "url": "/clicky/static/media/kermit3.eab3bf7f.png"
+  },
+  {
+    "revision": "a721eeccc1b7086d1876000af65afc75",
+    "url": "/clicky/static/media/kermit4.a721eecc.png"
+  },
+  {
+    "revision": "3cc1d4ca106630f5832b10e63f1ae4eb",
+    "url": "/clicky/static/media/kermit5.3cc1d4ca.png"
+  },
+  {
+    "revision": "fa8807127a8f89eb3f6823771fb099b9",
+    "url": "/clicky/static/media/kermit6.fa880712.png"
+  },
+  {
+    "revision": "ea67cda7cf6cbe77ba2ac235e118ad51",
+    "url": "/clicky/static/media/kermit7.ea67cda7.png"
+  },
+  {
+    "revision": "6c1c80c97134dc2c8c985edef3ac830b",
+    "url": "/clicky/static/media/kermit8.6c1c80c9.png"
+  },
+  {
+    "revision": "b6a3c1bfa3a4f048018b0ab3482e9bb8",
+    "url": "/clicky/static/media/kermit9.b6a3c1bf.png"
+  }
+]);
